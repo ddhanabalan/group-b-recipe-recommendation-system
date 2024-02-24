f43a875b12b63b1470407d263bb4cfb76b4776c5
safarh <safarh02122002@example.com>
2024-02-24 08:56:50 +0530

    added Items.jsx

---
+++ frontend/src/components/Items/Items.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import "./Items.css";
+const Items = () => {
+  return (
+    <div className="item">
+      {/*<img src={props.image} alt="" />
+      <p>{props.name}</p>
+      <div className="items-details">
+        <div className="mins">{props.mins}</div>
+        <div className="calories">{props.calories}</div>
+      </div>
+  <div className="item-rating">{props.items}</div>*/}
+      recipes
+    </div>
+  );
+};
+
+export default Items;
