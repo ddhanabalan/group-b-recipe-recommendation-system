4fc5540bb2250ad9364ae82d01c4359837688b3e
safarh <safarh02122002@example.com>
2024-02-26 22:06:53 +0530

    added popular recipes frame in home page

--- frontend/src/components/Popularrecipes/Popularrecipes.jsx
+++ frontend/src/components/Popularrecipes/Popularrecipes.jsx
@@ -1,8 +1,51 @@
 import React from "react";
 import "./Popularrecipes.css";
+import Items from "../Items/Items";
+import Carousel from "react-elastic-carousel";
 
-function Popularrecipes() {
-  return <div className="main">Popularrecipes</div>;
-}
+/*{
+  const breakPoints = [
+  {
+    width: 1,
+    itemsToShow: 1,
+  },
+  {
+    width: 558,
+    itemsToShow: 2,
+  },
+  {
+    width: 768,
+    itemsToShow: 3,
+  },
+  {
+    width: 1200,
+    itemsToShow: 3,
+  },
+]
+}*/
+
+export const Popularrecipes = () => {
+  return (
+    <div className="popular">
+      <h1>Popular Recipes</h1>
+      <div className="popularitem">
+        <Carousel
+          infinite={true}
+          itemsToShow={3}
+          pagination={false}
+          initialActiveIndex={3}
+        >
+          <Items />
+          <Items />
+          <Items />
+          <Items />
+          <Items />
+          <Items />
+          <Items />
+        </Carousel>
+      </div>
+    </div>
+  );
+};
 
 export default Popularrecipes;
