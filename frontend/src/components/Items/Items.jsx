448ac32456755577ec18453ee064f4d6d5ebfc5c
safarh <safarh02122002@example.com>
2024-02-28 23:44:19 +0530

    updated Items.jsx and css file

--- frontend/src/components/Items/Items.jsx
+++ frontend/src/components/Items/Items.jsx
@@ -1,16 +1,15 @@
 import React from "react";
 import "./Items.css";
-const Items = () => {
+const Items = (props) => {
   return (
     <div className="item">
-      {/*<img src={props.image} alt="" />
-      <p>{props.name}</p>
+      <img src={props.imageurl} alt="recipe" className="img" />
+      <h3>{props.title}</h3>
       <div className="items-details">
-        <div className="mins">{props.mins}</div>
-        <div className="calories">{props.calories}</div>
+        <div className="mins">{props.total_mins} mins</div>
+        <div className="calories">{props.calorie} Calorie</div>
       </div>
-  <div className="item-rating">{props.items}</div>*/}
-      recipes
+      <div className="item-rating">{props.ratings} Rating</div>
     </div>
   );
 };
