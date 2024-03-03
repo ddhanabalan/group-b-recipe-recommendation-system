49dce6c44addc5faa50c5b72726d345ea156162f
safarh <safarh02122002@example.com>
2024-03-03 21:25:48 +0530

    updated Item file

--- frontend/src/components/Items/Items.jsx
+++ frontend/src/components/Items/Items.jsx
@@ -1,15 +1,29 @@
 import React from "react";
 import "./Items.css";
+import LensIcon from "@mui/icons-material/Lens";
 const Items = (props) => {
   return (
     <div className="item">
       <img src={props.imageurl} alt="recipe" className="img" />
       <h3>{props.title}</h3>
       <div className="items-details">
-        <div className="mins">{props.total_mins} mins</div>
-        <div className="calories">{props.calorie} Calorie</div>
+        <div className="mins">
+          <b>{props.total_mins}</b> mins
+        </div>
+        <t>
+          <LensIcon
+            style={{
+              fontSize: 5,
+            }}
+          />
+        </t>
+        <div className="calories">
+          <b>{props.calorie} </b> Calorie
+        </div>
       </div>
-      <div className="item-rating">{props.ratings} Rating</div>
+      <div className="item-rating">
+        <b>{props.ratings}</b> Rating
+      </div>
     </div>
   );
 };
