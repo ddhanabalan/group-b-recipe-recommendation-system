b0d5671c902db6a17361cd8b972f6612f1965ced
safarh <safarh02122002@example.com>
2024-03-04 23:01:32 +0530

    updated home page

--- frontend/src/components/Customersays/Customersays.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-import "./Customersays.css";
-function Customersays() {
-  return <div className="main">Customersays</div>;
-}
-
-export default Customersays;
--- frontend/src/pages/Home/Home.jsx
+++ frontend/src/pages/Home/Home.jsx
@@ -87,22 +87,16 @@ function Home() {
       <Navbar />
       {/*Hero section*/}
       <div className="hero">
-        <div className="left">
-          <div className="left-heading">
-            <h1>Hello,</h1>
-            <h1>Are you Hungry?</h1>
-          </div>
-          <div className="left-intro">
-            <p>
-              Providing various easy-to-follow recipes from all over the
-              world.Learn how to make your cooking tastier and easier with us!
-            </p>
-          </div>
-          <div className="btn">
-            <Link to="/Recipe">
-              <div className="button">Explore all Recipes</div>
-            </Link>
-          </div>
+        <div className="left-heading">
+          <h1>Hello,</h1>
+          <h1>Are you Hungry?</h1>
+          <p>
+            Providing various easy-to-follow recipes from all over the
+            world.Learn how to make your cooking tastier and easier with us!
+          </p>
+          <Link to="/recipe">
+            <button active={true}>Explore All Recipes</button>
+          </Link>
         </div>
       </div>
       {/*popular recipes section*/}
