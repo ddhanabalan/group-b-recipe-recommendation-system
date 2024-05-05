35368a6b968fe45c121de6a0b08744c812452df0
safarh <safarh02122002@example.com>
2024-05-06 01:38:35 +0530

    changes made

--- frontend/src/context/sortContext.jsx
+++ frontend/src/context/sortContext.jsx
@@ -9,19 +9,28 @@ export const SortProvider = ({ children }) => {
   const sortRecipes = (option) => {
     setSortOption(option);
   };
+  // Inside SortContext.js
+
   const sortFunction = (a, b) => {
+    console.log("Sorting by:", sortOption); // Log the current sorting option
     switch (sortOption) {
       case "lowestCalorie":
+        console.log("Sorting by lowest calorie");
         return a.calories - b.calories;
       case "highestRating":
+        console.log("Sorting by highest rating");
         return b.rating - a.rating;
       case "alphabeticalAsc":
+        console.log("Sorting by alphabetical order (A-Z)");
         return a.title.localeCompare(b.title);
       case "alphabeticalDesc":
+        console.log("Sorting by alphabetical order (Z-A)");
         return b.title.localeCompare(a.title);
       case "leasttime":
+        console.log("Sorting by least time");
         return a.total_mins - b.total_mins;
       default:
+        console.log("Default sorting");
         return a.recipeid - b.recipeid;
     }
   };
